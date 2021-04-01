9a0097c92de9302d775f16b4e8475402814ffd31
MUHAMMAD REZA UTAMA PULUNGAN <rezapulungan@student.ub.ac.id>
2021-04-01 20:03:17 +0700

    first

---
+++ app/Http/Controllers/FirebaseController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Kreait\Firebase;
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
+
+class FirebaseController extends Controller
+{
+    public function index()
+    {
+
+        $serviceAccount = ServiceAccount::fromJsonFile(__DIR__ . '/FirebaseKey.json');
+
+        $firebase = (new Factory)
+            ->withServiceAccount($serviceAccount)
+            ->create();
+
+
+
+        $database = $firebase->getDatabase();
+
+        $ref = $database->getReference('users');
+
+        $key = $ref->push()->getKey();
+
+        return $key;
+    }
+}
